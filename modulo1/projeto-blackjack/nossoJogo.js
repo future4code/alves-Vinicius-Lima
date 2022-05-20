fd698feb7ceab73d2af2627061e884c6edbbc026
vinicius-peres <viniciusfula0409@gmail.com>
2022-05-20 17:04:58 -0300

    aula-projeto

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,56 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+   //   console.log(`Bem-vindo(a) ao jogo de Blackjack!`)
+
+   //   let jogo = confirm("Quer iniciar uma nova rodada?")
+   
+   //   if(jogo) {
+   //      let carta01Usuario = comprarCarta()
+   //      let carta02Usuario = comprarCarta()
+   //      let carta01Computador = comprarCarta()
+   //      let carta02Computador = comprarCarta()
+
+   //      let pontosUsuario = carta01Usuario.valor + carta02Usuario.valor
+   //      let pontosComputador = carta01Computador.valor + carta02Computador.valor
+
+   //      console.log(`Usuário - cartas: ${carta01Usuario.texto} ${carta02Usuario.texto} - ${pontosUsuario}`)
+   //      console.log(`Computador - cartas: ${carta01Computador.texto} ${carta02Computador.texto} - ${pontosComputador}`)
+
+   //      if(pontosUsuario > pontosComputador){
+   //         console.log(`Usuário ganhou!`)
+   //      } else if(pontosUsuario < pontosComputador) {
+   //         console.log(`Computador ganhou!`)
+   //      } else if(pontosUsuario === pontosComputador) {
+   //         console.log(`Empate!`)
+   //      }
+   //   } else {
+   //      console.log(`Jogo acabou!`)
+   //   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
