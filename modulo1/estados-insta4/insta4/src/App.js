a47129784bd36afccb15095831c952710e6db293
vinicius-peres <viniciusfula0409@gmail.com>
2022-06-09 19:07:38 -0300

    exercicios-09/06

--- modulo1/estados-insta4/insta4/src/App.js
+++ modulo1/estados-insta4/insta4/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
+import InfoUsuarios from './components/InfoUsuarios/InfoUsuarios'
+import AdicionarUsuario from './components/AdicionarUsuario/AdicionarUsuario';
 
 const MainContainer = styled.div`
   display: flex;
@@ -12,21 +14,9 @@ const MainContainer = styled.div`
 class App extends React.Component {
   render() {
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-        nomeUsuario={'marcelo'}
-        fotoUsuario={'https://www.albumdunyasi.com/application/uploads/2019/06/50x50-1-700x700.jpg'}
-        fotoPost={'https://i.redd.it/6ihl9vurecz01.png'}/>
-        <Post
-        nomeUsuario={'felipe'}
-        fotoUsuario={'https://www.fotodeky.cz/files/catalog-item/2003/2003324/catalog-item_2003324_img_15043899324935.jpg'}
-        fotoPost={'https://www.mall.cz/i/42535317/550/550'}/>
-      </MainContainer>
+      <>
+      <AdicionarUsuario/>
+      </>
     );
   }
 }
