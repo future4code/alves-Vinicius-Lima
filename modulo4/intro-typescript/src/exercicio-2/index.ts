0a0684ba875710e7861b2a85fe12f1b3dc0c7e7b
vinicius-peres <viniciusfula0409@gmail.com>
2022-08-10 17:16:19 -0300

    intro-typescript

---
+++ modulo4/intro-typescript/src/exercicio-2/index.ts
@@ -0,0 +1,8 @@
+function imprimeTresCoresFavoritas():void {
+    const cor1: string = ("Amarelo")
+    const cor2: string = ("Preto")
+    const cor3: string = ("Vermelho")
+    console.log([cor1, cor2, cor3])
+ }
+
+ console.log(imprimeTresCoresFavoritas())
