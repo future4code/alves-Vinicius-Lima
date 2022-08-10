dd82a66b98fd161c260da8118268433d75e3c1fb
vinicius-peres <viniciusfula0409@gmail.com>
2022-08-09 22:20:17 -0300

    node

---
+++ modulo4/node-package-json/exercicio-1/index.js
@@ -0,0 +1,5 @@
+// 1. A) Acessamos através da propriedade process.argv[]
+
+// B,C) console.log("Olá,", process.argv[2],"!","Você tem", process.argv[3],"anos.", "Em sete anos você tera", process.argv[4], "anos.")
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ modulo4/node-package-json/exercicio-2/index.js
@@ -0,0 +1,10 @@
+// 2. const valores = process.argv[2]
+
+// const subtracao = valores - process.argv[3]
+// const multiplicacao = valores * process.argv[3]
+// const divisao = valores / process.argv[3]
+// const soma = valores + process.argv[3]
+
+// console.log("soma:", soma, "subtração:", subtracao, "multiplicação:", multiplicacao, "divisão:", divisao)
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
